35a5179c25459f199775a2ad6246eedbdf6854b9
soomsoomee <soominn20@gmail.com>
2022-06-04 13:54:27 +0900

    220604

--- c++_coding/CList.h
+++ c++_coding/CList.h
@@ -46,9 +46,104 @@ public:
 	int size() { return m_iCount; }
 
 public:
+	class iterator;
+	iterator begin();
+	iterator end();
+	iterator erase();
+	iterator insert(const iterator& _iter, const T& _data);
+
+public:
 
 	CList();
 	~CList();
+
+
+	class iterator
+	{
+
+	private:
+		CList<T>*         m_pList;
+		tListNode<T>*     m_pNode; // null인 경우, end iterator로 간주
+		bool              m_bValid;
+
+
+	public:
+		T& operator* ()
+		{
+			return m_pNode->data;
+		}
+
+		bool operator ==(const iterator& _otheriter)
+		{
+			if (m_pList == _otheriter.m_pList && m_pNode == _otheriter.m_pNode)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		bool operator !=(const iterator& _otheriter)
+		{
+			return !(*this == _otheriter);
+		}
+
+		iterator operator ++()
+		{
+			// end에서 ++한 경우
+			if (nullptr == m_pNode || !m_bValid)
+			{
+				assert(nullptr);
+			}
+			m_pNode = m_pNode->pNext;
+
+			return *this;
+		}
+
+		iterator operator ++(int)
+		{
+			iterator copyiter(*this);
+
+			++(*this);
+
+			return copyiter;
+
+		}
+
+		iterator operator --()
+		{
+
+		}
+
+		iterator operator --(int)
+		{
+
+		}
+
+	public:
+		iterator()
+			: m_pList(nullptr)
+			, m_pNode(nullptr)
+			, m_bValid(false)
+		{
+		};
+		iterator(CList<T>* _pList, tListNode<T>* _pNode)
+			: m_pList(_pList)
+			, m_pNode(_pNode)
+			, m_bValid(false)
+		{
+			if (nullptr != _pList && nullptr != _pNode)
+			{
+				m_bValid = true;
+			}
+		};
+
+		~iterator()
+		{
+
+		};
+
+		friend class CList;
+	};
 	 
 
 };
@@ -103,7 +198,65 @@ inline void CList<T>::push_front(const T& _data)
 	m_pHead = pNewNode;
 
 	++m_iCount;
-};
+}
+
+template<typename T>
+inline typename CList<T>::iterator CList<T>::begin()
+{
+	return iterator(this, m_pHead);
+}
+
+
+template<typename T>
+inline typename CList<T>::iterator CList<T>::end()
+{
+	// 리스트가 비어있을 때 begin으로 받아온 iterator를 end로 본다.
+	// 리스트 비어있으면 헤드 노드는 nullptr
+	// 마지막 노드 다음도 nullptr
+	return iterator(this, nullptr);
+}
+
+template<typename T>
+inline typename CList<T>::iterator CList<T>::erase()
+{
+	return iterator();
+}
+
+
+template<typename T>
+inline typename CList<T>::iterator CList<T>::insert(const iterator& _iter, const T& _data)
+{
+	if (end() == _iter)
+	{
+		assert(nullptr);
+	}
+
+	// 리스트에 추가되는 데이터를 저장할 Node 생성
+	tListNode<T>* pNode = new tListNode<T>(_data, nullptr, nullptr);
+
+	// iterator가 가리키는 노드가 헤드 노드인 경우
+	if (_iter.m_pNode == m_pHead)
+	{
+		_iter.m_pNode->pPrev = pNode;
+		m_pHead = pNode;
+	}
+	else
+	{
+		// iterator가 가리키고 있는 노드의 이전으로 가서 다음 노드 주소를 새로 생성한 노드로 지정
+		_iter.m_pNode->pPrev->pNext = pNode;
+		pNode->pPrev = _iter.m_pNode->pPrev;
+		
+		// iterator가 가리키고 있는 노드의 이전을 새로운 노드로 지정
+		// 새로운 노드의 pNext를 iterator가 가리키고 있는 노드로 지정
+		_iter.m_pNode->pPrev = pNode;
+		pNode->pNext = _iter.m_pNode;
+	}
+	
+	++m_iCount;
+
+	return iterator(this, pNode);
+}
+;
 
 template<typename T>
 CList<T>:: ~CList()
